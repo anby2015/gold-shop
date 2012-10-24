fcb9a72c3d2441be8ba8d9d402dd990f7fdbfca7
liubin123@gmail.com <liubin123@gmail.com@f065bbae-b7ec-3373-67c3-b154bb10f89d>
2012-10-24 14:48:47 +0000

    add menu,navigation,Separate index.php

---
+++ trunk/web/menu.php
@@ -0,0 +1,15 @@
+<TR>
+<TD align=middle><a href="index.htm" target="_parent">主页</a></TD>
+<TD align=middle width=30>|</TD>
+<TD align=middle><a href="jdgk.htm" target="_parent">金店概况</TD>
+<TD align=middle width=30>|</TD>
+<TD align=middle><a href="jyxm.htm" target="_parent">经营项目<a></TD>
+<TD align=middle width=30>|</TD>
+<TD align=middle><a href="cpzs.htm" target="_parent">产品展示</a></TD>
+<TD align=middle width=30>|</TD>
+<TD align=middle><a href="scjz.htm" target="_parent">收藏价值</a></TD>
+<TD align=middle width=30>|</TD>
+<TD align=middle><a href="jbjq.htm" target="_parent">鉴别技巧</a></TD>
+<TD align=middle width=30>|</TD>
+<TD align=middle><a href="zxfk.htm" target="_parent">在线反馈</a></TD>
+</TR>
---
+++ trunk/web/navigation.php
@@ -0,0 +1,27 @@
+<TR>
+<TR>
+<TD align=middle><A href="#" target="_top">婚庆系列</A></TD>
+</TR>
+<TR>
+<TD align=middle height=20></TD>
+<TR>
+<TD align=middle><A href="#" target="_top">情侣系列</A></TD>
+</TR>
+<TR>
+<TD align=middle height=20></TD>
+<TR>
+<TD align=middle><A href="#" target="_top">宝宝系列</A></TD>
+</TR>
+<TR>
+<TD align=middle height=20></TD>
+<TR>
+<TD align=middle><a href="#" target="_top">投资系列</a></TD>
+</TR>
+<TR>
+<TD align=middle height=20></TD>
+<TR>
+<TD align=middle><p><a href="#" target="_top">收藏系列</a></p>
+</TD>
+</TR>
+<TR>
+<TD align=middle height=0></TD></TR>
