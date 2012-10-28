55d53b292342ebafc51429bb323b3f0efe0eab99
liubin123 <liubin123@gmail.com>
2012-10-28 07:50:30 +0000

    add guest_book

---
+++ web/guest_book.php
@@ -0,0 +1,454 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML><HEAD><TITLE>满宏金店欢迎您！</TITLE>
+<META http-equiv=Content-Type content="text/html; charset=gb2312">
+<META content=undefined name=description>
+<META content=满宏金店 金店 榆林金店 name=keywords>
+<META name=searchtitle content=金店>
+<META content=all name=robots><LINK href="index.files/style.css" 
+type=text/css rel=stylesheet>
+<STYLE type=text/css>BODY {
+	MARGIN: 0px
+}
+</STYLE>
+
+<META content="MSHTML 6.00.2900.3243" name=GENERATOR></HEAD>
+<BODY background=index.files/bg_index.gif>
+<CENTER>
+<TABLE cellSpacing=0 cellPadding=0 width=1003 border=0>
+  <TBODY>
+  <TR>
+    <TD width=52></TD>
+    <TD width=900 background=index.files/user3-bg.gif>
+      <TABLE height=70 cellSpacing=0 cellPadding=0 width=900 border=0>
+        <TBODY>
+        <TR>
+          <TD width=300 background=index.files/logo.jpg>
+			<IMG src="index.files/logo.jpg">
+		  </TD>
+          <TD align=right width=600></TD>
+		</TR>
+		</TBODY>
+	  </TABLE>
+	</TD>
+    <TD width=51></TD>
+  </TR>
+  <TR>
+    <TD background=index.files/user1-left.gif></TD>
+    <TD vAlign=center align=middle width=900 background=index.files/topmenu-bg.gif height=61>
+      <DIV class="top topmenu">
+      <TABLE class="top topmenu" cellSpacing=0 cellPadding=0 border=0>
+        <TBODY>
+          <?php require("menu.php"); ?>
+		</TBODY>
+	  </TABLE>
+      </DIV>
+	</TD>
+    <TD background=index.files/user1-right.gif></TD></TR>
+  <TR>
+    <TD background=""></TD>
+    <TD width=900 background=index.files/banner-bg.jpg height=200>
+      <TABLE height=200 cellSpacing=0 cellPadding=0 width=900 background="" border=0>
+        <TBODY>
+        <TR>
+          <TD vAlign=center align=middle width=195 background=index.files/user1-bg.gif>
+            <DIV class="user1 mainmenu">
+            <TABLE class="user1 mainmenu" cellSpacing=0 cellPadding=0 border=0>
+			<TBODY>
+              <?php require("navigation.php"); ?>
+			</TBODY>
+			</TABLE>
+			</DIV>
+		  </TD>
+          <TD vAlign=top align=left width=705 background=index.files/banner-bg.jpg>
+            <TABLE class=banner cellSpacing=0 cellPadding=0 width="100%" border=0>
+              <TBODY>
+                <TR>
+                  <TD align=middle></TD>
+			    </TR>
+			  </TBODY>
+			</TABLE>
+            <TABLE class="banner userdesign" cellSpacing=0 cellPadding=0 width="100%" border=0>
+              <TBODY>
+              <TR>
+                <TD><object classid=clsid:D27CDB6E-AE6D-11cf-96B8-444553540000 
+                  codebase=http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0 width=705 height=198 align="right">
+                  <param name="_cx" value="18653">
+                  <param name="_cy" value="5292">
+                  <param name="FlashVars" value="">
+                  <param name="Src" value="index.files/banner.swf">
+                  <param name="WMode" value="Transparent">
+                  <param name="Play" value="-1">
+                  <param name="Loop" value="-1">
+                  <param name="Quality" value="high">
+                  <param name="SAlign" value="">
+                  <param name="Menu" value="-1">
+                  <param name="Base" value="">
+                  <param name="AllowScriptAccess" value="">
+                  <param name="DeviceFont" value="0">
+                  <param name="EmbedMovie" value="0">
+                  <param name="BGColor" value="">
+                  <param name="SWRemote" value="">
+                  <param name="MovieData" value="">
+                  <param name="SeamlessTabbing" value="1">
+                  <param name="Profile" value="0">
+                  <param name="ProfileAddress" value="">
+                  <param name="ProfilePort" value="0">
+                  <param name="AllowNetworking" value="all">
+                  <embed src="index.files/banner.swf" width="705" height="198" align="right" quality=high 
+                  pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" 
+                  type="application/x-shockwave-flash"></embed>
+                </object></TD>
+              </TR>
+			  </TBODY>
+			</TABLE>
+		  </TD></TR>
+		</TBODY>
+	  </TABLE>
+	</TD>
+    <TD></TD></TR>
+  </TBODY>
+</TABLE>
+<TABLE cellSpacing=0 cellPadding=0 width=1003 border=0>
+  <TBODY>
+  <TR>
+    <TD width=52></TD>
+    <TD width=210></TD>
+    <TD width=20></TD>
+    <TD width=670></TD>
+    <TD width=51></TD>
+  </TR>
+  <TR>
+    <TD width=52 height=450></TD>
+    <TD vAlign=top align=middle bgColor=#610600 height=450>
+      <TABLE style="BORDER-COLLAPSE: collapse" borderColor=#a59c7b height="100%" width=210 border=1>
+        <TBODY>
+        <TR>
+          <TD vAlign=center align=middle height=30></TD></TR>
+        <TR>
+          <TD vAlign=top align=middle>
+            <TABLE cellSpacing=0 cellPadding=0 width=194 border=0>
+              <TBODY>
+              <TR>
+                <TD>
+                  <TABLE class="HomeAreaA HomeAreaA01 userlogin" cellSpacing=0 
+                  cellPadding=0 width="100%" border=0>
+                    <THEAD>
+                    <TR>
+                      <TD>会员登录</TD>
+					</TR>
+					</THEAD>
+                    <TBODY></TBODY>
+				  </TABLE>
+				</TD>
+			  </TR>
+              <TR>
+                <TD height=6></TD></TR>
+              <TR>
+                <TD>
+                  <DIV class="HomeAreaA HomeAreaA01 userlogin">
+                  <TABLE class="HomeAreaA HomeAreaA01 userlogin" cellSpacing=0 cellPadding=0 width="100%" border=0>
+                    <TBODY>
+                    <FORM name=form2 action=index.php?option=com_member&amp;task=login&amp;template=1&amp;menuid=1&amp;module=&amp;sortid=&amp;artid= 
+                    method=post>
+                    <TR>
+                      <TD>登录名：<INPUT class=text size=12 name=UserName><BR>
+					      密　码：<INPUT class=text type=password size=12 name=Code><BR> 
+                        &nbsp;
+                        <input type="submit" name="Submit" value="登录">
+                        <input type="submit" name="Submit" value="注册">
+                        <BR>
+                        <SPAN class=UserItem>忘记密码</SPAN><BR>
+					  </TD>
+					</TR>
+					</FORM>
+				  </TABLE>
+				  </DIV>
+                  <DIV align=right></DIV>
+				</TD>
+			  </TR>
+              <TR>
+                <TD height=6></TD>
+			  </TR>
+			</TABLE>
+            <TABLE cellSpacing=0 cellPadding=0 width=194 border=0>
+              <TBODY></TBODY>
+			</TABLE>
+            <TABLE cellSpacing=0 cellPadding=0 width=194 border=0>
+              <TBODY>
+              <TR>
+                <TD>
+                  <TABLE class="HomeAreaA HomeAreaA03 website" cellSpacing=0 cellPadding=0 width="100%" border=0>
+                    <THEAD>
+                    <TR>
+                      <TD>今日金价</TD>
+                    </TR>
+					</THEAD>
+                    <TBODY></TBODY>
+				  </TABLE>
+				</TD>
+			  </TR>
+              <TR>
+                <TD height=6></TD>
+			  </TR>
+              <TR>
+                <TD>
+                  <DIV class="HomeAreaA HomeAreaA03 website">
+                  <TABLE class="HomeAreaA HomeAreaA03 website" cellSpacing=0 cellPadding=0 width="100%" border=0>
+                    <TBODY>
+                    <TR>
+                      <TD width="100%">&nbsp;</TD>
+                    </TR>
+                    <TR>
+                      <TD width="100%">&nbsp;</TD>
+                    </TR>
+                    <TR>
+                      <TD width="100%">&nbsp;</TD>
+                    </TR>
+                    <TR>
+                      <TD width="100%">&nbsp;</TD>
+                    </TR>
+                    <TR>
+                      <TD width="100%">&nbsp;</TD>
+                    </TR>
+                    <TR></TR>
+					</TBODY>
+				  </TABLE>
+				  </DIV>
+                  <DIV align=right></DIV>
+				</TD>
+			  </TR>
+              <TR>
+                <TD height=6></TD>
+			  </TR>
+			  </TBODY>
+			</TABLE>
+            <TABLE cellSpacing=0 cellPadding=0 width=194 border=0>
+              <TBODY>
+              <TR>
+                <TD>
+                  <TABLE class="HomeAreaA HomeAreaA04 sitelink" cellSpacing=0 cellPadding=0 width="100%" border=0>
+                    <THEAD>
+                    <TR>
+                      <TD>友情链接</TD>
+					</TR>
+					</THEAD>
+                    <TBODY></TBODY>
+				  </TABLE>
+				</TD>
+			  </TR>
+              <TR>
+                <TD height=6></TD>
+			  </TR>
+              <TR>
+                <TD>
+                  <DIV class="HomeAreaA HomeAreaA04 sitelink">
+                  <TABLE class="HomeAreaA HomeAreaA04 sitelink" cellSpacing=0 cellPadding=0 width="100%" border=0>
+                    <TBODY>
+                    <TR>
+                      <TD width="100%">&nbsp;</TD>
+                    </TR>
+                    <TR>
+                      <TD width="100%">&nbsp;</TD>
+                    </TR>
+                    <TR>
+                      <TD width="100%">&nbsp;</TD>
+                    </TR>
+                    <TR></TR></TBODY>
+				  </TABLE>
+				  </DIV>
+                  <DIV align=right></DIV>
+				</TD>
+			  </TR>
+              <TR>
+                <TD height=6></TD>
+			  </TR>
+			  </TBODY>
+			</TABLE>
+            <TABLE cellSpacing=0 cellPadding=0 width=194 border=0>
+              <TBODY>
+              <TR>
+                <TD>
+                  <TABLE class="HomeAreaA HomeAreaA05 vote" cellSpacing=0 cellPadding=0 width="100%" border=0>
+                    <THEAD>
+                    <TR>
+                      <TD>网站调查</TD></TR></THEAD>
+                    <TBODY></TBODY>
+				  </TABLE>
+				</TD>
+			  </TR>
+              <TR>
+                <TD height=6>收藏黄金</TD>
+              </TR>
+              <TR>
+                <TD>
+                  <DIV class="HomeAreaA HomeAreaA05 vote">
+                  <TABLE class="HomeAreaA HomeAreaA05 vote" cellSpacing=0 cellPadding=0 width="100%" border=0>
+                    <FORM name=votefrom1 action=index.php?option=com_vote&amp;menuid=1&amp;voteid=1 method=post>
+                    <TBODY>
+                    <TR>
+                      <TD>
+                        <DIV 
+                        class="HomeAreaA HomeAreaA05 VoteDes">
+                          <blockquote>
+                            <p>&nbsp;</p>
+                          </blockquote>
+                        </DIV></TD></TR>
+                    <TR>
+                      <TD align=left><INPUT type=radio value=1 
+                        name=option1>
+                        </TD>
+                    </TR>
+                    <TR>
+                      <TD align=left><INPUT type=radio value=2 
+                        name=option1>
+                        </TD>
+                    </TR>
+                    <TR>
+                      <TD align=left><INPUT type=radio value=3 
+                        name=option1></TD></TR>
+                    <TR>
+                      <TD align=left><INPUT type=radio value=4 
+                        name=option1>
+                        </TD>
+                    </TR>
+					</TBODY>
+                    <TFOOT>
+                    <TR>
+                      <TD vAlign=center align=middle colSpan=2 height=36>
+					  <INPUT class=button type=submit value=投票 name=Submit>&nbsp;
+					  <INPUT class=button type=reset value=重置 name=reset>&nbsp;
+					  <INPUT class=button onClick="javascript:location.replace('index.php?option=com_vote&amp;menuid=1&amp;voteid=1');" type=button value=查看 name=view>
+					  <INPUT type=hidden value=option1 name=optname><BR>
+					  </TD>
+					</TR>
+					</TFOOT>
+					</FORM>
+                  </TABLE>
+				  </DIV>
+                  <DIV align=right></DIV>
+				</TD>
+			  </TR>
+              <TR>
+                <TD height=6></TD>
+			  </TR>
+			  </TBODY>
+			</TABLE>
+            <TABLE cellSpacing=0 cellPadding=0 width=194 border=0>
+              <TBODY></TBODY>
+			</TABLE>
+		  </TD>
+		</TR>
+        <TR>
+          <TD vAlign=center align=middle background="" height=30></TD>
+		</TR>
+	  </TABLE>
+	</TD>
+    <TD></TD>
+    <TD vAlign=top align=left width=670 background="" bgColor=#7f0b04 height=450>
+      <TABLE style="BORDER-COLLAPSE: collapse" borderColor=#a59c7b height="100%" width=670 background="" border=1>
+        <TBODY>
+        <TR>
+          <TD background="" height=30>
+            <TABLE class="user6 userdesign" cellSpacing=0 cellPadding=0 width="100%" border=0>
+              <TBODY>
+              <TR>
+                <TD>
+                  <MARQUEE>满宏金店欢迎您的到来，祝您愉快！联系电话：0912-3467889；手机：13992252518，13772395923</MARQUEE>
+				</TD>
+			  </TR>
+			  </TBODY>
+			</TABLE>
+		   </TD>
+		</TR>
+        <TR>
+          <TD vAlign=top align=middle background="">
+            <TABLE cellSpacing=0 cellPadding=0 width=634 border=0>
+              <TBODY>
+              <TR>
+                <TD vAlign=top></TD>
+			  </TR>
+			  </TBODY>
+			</TABLE>
+			<!--display guest book-->
+			<TABLE cellSpacing=0 cellPadding=0 width=634 border=1>
+			Welcome <?php echo $_POST["name"]; ?>.<br />
+			Your phone num <?php echo $_POST["phone"]; ?> <br/>
+			Your qq num <?php echo $_POST["qq"]; ?> <br/>
+			Your content  <?php echo $_POST["content"]; ?> <br/>
+			</TABLE>
+		   </TD>
+		</TR>
+        <TR>
+          <TD background="" height=30></TD>
+		</TR>
+		</TBODY>
+	  </TABLE></TD>
+    <TD></TD></TR>
+  <TR>
+    <TD width=52 background="" height=25></TD>
+    <TD width=210 background="" height=25></TD>
+    <TD width=20 background="" height=25></TD>
+    <TD width=670 background="" height=25></TD>
+    <TD width=51 background="" height=25></TD>
+  </TR></TBODY></TABLE>
+<TABLE height=107 cellSpacing=0 cellPadding=0 width=1003 background=index.files/copyright-bg.gif border=0>
+  <TBODY>
+  <TR>
+    <TD width=52 background="" height=1></TD>
+    <TD width=900 background="" height=1></TD>
+    <TD width=51 background="" height=1></TD></TR>
+  <TR>
+    <TD width=52></TD>
+    <TD vAlign=top align=middle width=900 background="">
+	<TABLE height="100%" cellSpacing=0 cellPadding=0 width=900 border=0>
+        <TBODY>
+        <TR>
+          <TD align=middle background=index.files/user2-bg.gif height=49>
+            <DIV class="user2 bottommenu">
+            <TABLE class="user2 bottommenu" cellSpacing=0 cellPadding=0 border=0>
+              <TBODY>
+              <TR>
+                <TD align=middle><a href="#">员工登陆</a></TD>
+                <TD align=middle width=35>|</TD>
+                <TD align=middle>
+					<A href="#">联系我们</A>
+				</TD>
+                <TD align=middle width=0></TD>
+			  </TR>
+			  </TBODY>
+			</TABLE>
+			</DIV>
+		   </TD>
+		</TR>
+        <TR>
+          <TD align=middle background="">
+            <TABLE cellSpacing=2 cellPadding=3 width="100%" border=0>
+              <TBODY>
+              <TR>
+                <TD>&nbsp;</TD>
+              </TR>
+              <TR>
+                <TD>
+                  <P align=center>版权所有：<a href="index.htm">满宏金店</a>　 地址：榆林市镇川镇北大街37号　 2008年元月<BR>
+                  </P>
+				</TD>
+			  </TR>
+              <TR>
+                <TD>
+                  <P align=center>&nbsp;</P>
+				</TD>
+			  </TR>
+			  </TBODY>
+			</TABLE>
+		   </TD>
+		</TR>
+		</TBODY>
+	</TABLE>
+	</TD>
+    <TD width=51 background=""></TD>
+	</TR>
+	</TBODY>
+	</TABLE>
+	</CENTER>
+</BODY>
+</HTML>
